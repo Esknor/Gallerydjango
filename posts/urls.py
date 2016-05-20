cf8eb485a4f293529e8bc21e1006020c55535629
Esknor <vldaimir2016@mail.ua>
2016-05-20 10:04:51 -0300

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -6,11 +6,13 @@ from . views import (
   post_detail,
   post_update,
   post_delete,
+  title,
 )
 
 urlpatterns = [
-  url(r'^$', post_list, name='post_list'),
-  url(r'^create/$', post_create, name='post_create'),
+  url(r'^$', title, name='title'),
+  url(r'^post_list/$', post_list, name='post_list'),
+  url(r'^post_create/$', post_create, name='post_create'),
   url(r'^(?P<slug>[\w-]+)/$', post_detail, name='post_detail'),
   url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='post_update'),
   url(r'^(?P<slug>[\w-]+)/delete/$', post_delete, name='post_delete'),
